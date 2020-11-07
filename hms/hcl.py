18ff77aed477bd4fc19d935d4d9890270321d6fa
leesh7672 <leesh7672@gmail.com>
2020-11-07 21:01:19 +0900

    A new system of word category has been adopted.

--- hms/hcl.py
+++ hms/hcl.py
@@ -7,6 +7,7 @@ c = Cihai()
 
 import hms.tex as tex
 import hms.html as html
+import psycopg2
 latest_ident = 0
 
 def generateIdent():
@@ -63,26 +64,18 @@ def scandef(e, spell, ident, coder=tex):
     else:
         num = e.attrib['num']
     for child in e:
-        if child.tag == 'pronoun':
-            category = '指詞'
         if child.tag == 'noun':
-            category = '稱詞'
+            category = '名稱詞'
         elif child.tag == 'verb':
-            category = '謂詞'
-        elif child.tag == 'adnoun':
-            category = '冠詞'
+            category = '行動詞'
+        elif child.tag == 'adjective':
+            category = '被狀詞'
+        elif child.tag == 'modifier':
+            category = '冠修詞'
         elif child.tag == 'interjection':
-            category = '吟詞'
-        elif child.tag == 'connecter':
-            category = '束詞'
-        elif child.tag == 'adverb':
-            category = '修詞'
-        elif child.tag == 'final':
-            category = '結詞'
-        elif child.tag == 'coverb':
-            category = '縛詞'
+            category = '詠歎詞'
         elif child.tag == 'particle':
-            category = '助詞'
+            category = '空虛詞'
         elif child.tag == 'exp':
             explanation = textify(child, spell, ident, coder)
         elif child.tag == 'samp':
@@ -211,17 +204,17 @@ def collect_entries(code=tex):
                 self.values =values
             def index_spell(self):
                 return _spell(self.values)
-    update()
-    results = []
-    for (path, dir, files) in os.walk('./'):
-        for filename in files:
-            p = '{}/{}'.format(path, filename)
-            ext = os.path.splitext(filename)[-1]
-            if ext == '.xml':
-                result = scanxml(elemTree.parse(p))
-                results += [entry(result)]
-    results.sort(key=methodcaller('index_spell'))
-    return results
+        update()
+        results = []
+        for (path, dir, files) in os.walk('./'):
+            for filename in files:
+                p = '{}/{}'.format(path, filename)
+                ext = os.path.splitext(filename)[-1]
+                if ext == '.xml':
+                    result = scanxml(elemTree.parse(p))
+                    results += [entry(result)]
+        results.sort(key=methodcaller('index_spell'))
+        return results
 def build_db(conn):
     results = collect_entries(html)
     conn.execute("DROP TABLE IF EXISTS _alternative_spells;")
