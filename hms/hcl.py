0e9eefa8c9a1e1ed9e979cc0aaa6751c03f8a938
Seho Lee <leesh7672@gmail.com>
2022-02-16 08:12:39 +0900

    An error has been repaired.

--- hms/hcl.py
+++ hms/hcl.py
@@ -236,7 +236,7 @@ def collect_entries(code=tex):
                 ext = os.path.splitext(filename)[-1]
                 if ext == '.xml':
                     results += [scanxml(etree.parse(p))]
-        results.sort(key=lambda x: _spell(x[2], x[1]))
+        results.sort(key=lambda x: _spell(x[3], x[2]))
         return results
 def build_db(conn):
     results = collect_entries(html)
