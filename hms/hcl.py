a4a6da83d41e448915b2fbe7bf0e469b46a5b2f7
Seho Lee <leesh7672@gmail.com>
2022-04-21 22:42:43 +0900

    A new system to categorize words are adopted.

--- hms/hcl.py
+++ hms/hcl.py
@@ -60,8 +60,8 @@ def textify(e, spell, ident, coder=tex):
             beforehand = True
     return total.strip()
 categories = {'op':"結詞", 'comp':"成詞", 'infl':"助詞", 'adv':"副詞",
-    'lv': "助動詞", 'verb': "動詞", 'prep': "介詞",
-    'co': "連詞", 'det': "指詞", 'adj': "定詞", 'noun': "名詞",
+    'verb': "動詞", 'prep': "介詞",
+    'co': "連詞", 'det': "指詞", 'adj': "性詞", 'noun': "名詞",
     'cl': "量詞", 'num': "數詞"}
 def scandef(e, spell, ident, coder=tex):
     synonyms = []
