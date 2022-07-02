b59262218a6faf28f8db4adda55cd894c15aed6b
Seho Lee <leesh7672@gmail.com>
2022-07-03 06:26:20 +0900

    An error has been repaired.

--- hms/hcl.py
+++ hms/hcl.py
@@ -195,7 +195,7 @@ def updatexml(path):
     else:
         num = root.attrib['num']
     ident = root.attrib['ident']
-    etree.ElementTree(tree.getroot()).write(path, encoding='utf-8')
+    etree.ElementTree(tree.getroot()).write(path, pretty_print=True, encoding='utf-8')
 def scanxml(tree):
     root = tree.getroot()
     num = root.attrib['num']
