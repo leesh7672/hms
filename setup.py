56b78f94e03f24b2e3087c25de47a6aa3b10fc44
leesh7672 <leesh7672@gmail.com>
2022-11-12 02:18:53 +0900

    An error has been repaired.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def long_description():
 
 setup(
     name='hms',
-    version='3.2',
+    version='3.4',
     description='HCL Management software rewritten into python.',
     long_description=long_description(),
     author='Seho Lee',
