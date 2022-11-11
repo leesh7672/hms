34922de883325c4acb58affa3bf0aae9d906fe5b
leesh7672 <leesh7672@gmail.com>
2022-11-12 03:03:52 +0900

    An error has been repaired.

--- hms/hcl.py
+++ hms/hcl.py
@@ -25,7 +25,7 @@ def xp(e):
             children += [xp(child)]
         if child.tag == "xp-text":
             xp_text = textify(child)
-            children += [(child.attrib['category'], xp_text, f"[{categories[child.attrib[category]]}組 [{xp_text}, roof]]")]
+            children += [(child.attrib['category'], xp_text, f"[{categories[child.attrib["category"]]}組 [{xp_text}, roof]]")]
         elif child.tag == "xbar":
             grandchildren = []
             xbar_text = ""
@@ -35,7 +35,7 @@ def xp(e):
                     grandchildren += [xp(grandchild)]
                 elif grandchild.tag == "xp-text":
                     xp_text = textify(grandchild)
-                    grandchildren += [(grandchild.attrib['category'], xp_text, f"[{categories[grandchild.attrib[category]]}組 [{xp_text}, roof]]")]
+                    grandchildren += [(grandchild.attrib['category'], xp_text, f"[{categories[grandchild.attrib["category"]]}組 [{xp_text}, roof]]")]
                 elif grandchild.tag == "x-text":
                     spell = textify(grandchild)
                     grandchildren += [(category, spell, f"[{categories[category]} [{spell}]]")]
