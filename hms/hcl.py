fefbd3e0c7972cd9a58dfb67ba7acff742396dab
leesh7672 <leesh7672@gmail.com>
2022-11-12 03:52:17 +0900

    An error has been repaired.

--- hms/hcl.py
+++ hms/hcl.py
@@ -70,7 +70,7 @@ def textify(e, coder=tex):
             total += '（又曰、{}）'.format(temp)
             beforehand = False
         elif child.tag == 'xp':
-            total += "\\begin{{forest}}for tree={{if n children=0{{font=\itshape,tier=terminal,}}{{}},}}{}\\end{{forest}}".format(xp(child)[2])
+            total += "\\begin{{forest}}{}\\end{{forest}}".format(xp(child)[2])
             beforehand = False
         elif child.tag == 'quote':
             temp = textify(child, coder)
